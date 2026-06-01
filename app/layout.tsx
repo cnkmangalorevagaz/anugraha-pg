692fc3d4ee92ea3aac39a30fc4cd9cda168968c4
cnkmangalorevagaz <cnkmangalore@gmail.com>
2026-06-01 13:59:53 +0530

    Updated Anugraha PG website

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
       "Safety and Comfort, Crafted for Her",
     images: [
       {
-        url: "/ogimage.png",
+        url: "images/ogimage.png",
         width: 1200,
         height: 630,
       },
